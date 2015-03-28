7cdc64ff66921876ab815e107683dcc245654a74
Bernhard Rieder <rieder@MyMBA.local>
2015-03-28 07:46:31 +0100

    update

--- conf_example.php
+++ conf_example.php
@@ -2,7 +2,7 @@
 
 // to get the following variables, register a client at http://instagram.com/developer/clients/register/
 
-$apiKey = "";
+$apiKey = ""; 
 $apiSecret = "";
 $apiCallback = "";			// must point to success.php and be identical to the redirect URI specified in the client manager on Instagram
 
