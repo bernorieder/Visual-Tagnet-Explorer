b7ad152922624193f69514cb765bcfb72e75c1df
Bernhard Rieder <rieder@MyMBA.local>
2015-04-09 11:37:36 +0200

    bugfixes

--- conf_example.php
+++ conf_example.php
@@ -1,5 +1,7 @@
 <?php
 
+$base_url = "http://yourserver.com/yourdirectory/";
+
 // to get the following variables, register a client at http://instagram.com/developer/clients/register/
 
 $apiKey = "";
