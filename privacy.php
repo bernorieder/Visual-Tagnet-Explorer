0acfda52ab1ca5fc60b7dc2cef23ebe71f99a6c9
bernorieder <berno.rieder@gmail.com>
2017-01-31 13:20:46 +0000

    post API changes version

---
+++ privacy.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Visual Tagnet Explorer</title>
+	
+	<link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+
+<body>
+
+<table>
+	
+	<tr>
+		<td >
+			
+		<h1>Visual Tagnet Explorer</h1>
+	
+		<h2>Privacy Policy</h2>
+		
+		<p>This app uses your Instagram credentials to connect to the service's API to retrieve lists of media that are either tagged with a specific hashtag or posted to a specific location. It does not retrieve any info from your personal account and it
+			does not post from your account in any way. No account information or other elements (IP address, etc.) are stored and the data files you generate are automatically deleted from the server once per day. 
+		</p>
+		
+		<p>The full source code of this app is available <a href="https://github.com/bernorieder/Visual-Tagnet-Explorer" target="_blank">here.</a></p>
+		
+		</td>
+	</tr>
+</table>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-68146659-2', 'auto');
+  ga('send', 'pageview');
+</script>
+
+</body>
+</html>
